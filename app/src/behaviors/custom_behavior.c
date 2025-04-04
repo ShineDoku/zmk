05f9b95b16804b20efb214fc5a9db90699da9804
ShineDoku <acidka1997@gmail.com>
2025-04-04 18:10:19 +0300

    Update custom_behavior.c

--- app/src/behaviors/custom_behavior.c
+++ app/src/behaviors/custom_behavior.c
@@ -4,9 +4,9 @@
 static void custom_macro_hold(struct zmk_behavior_binding *binding) {
     while (zmk_behavior_is_held(binding)) {
         zmk_macro_tap_key(J_KEY); // Отправляем клавишу J
-        k_msleep(5);              // Задержка 5 миллисекунд
+        k_msleep(150);              // Задержка 5 миллисекунд
         zmk_macro_tap_key(K_KEY); // Отправляем клавишу K
-        k_msleep(5);              // Задержка 5 миллисекунд
+        k_msleep(150);              // Задержка 5 миллисекунд
     }
 }
 static void custom_macro_press(struct zmk_behavior_binding *binding) {}

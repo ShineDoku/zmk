5b475c5a1416976e796cb3f72c48fe1993366a7c
ShineDoku <acidka1997@gmail.com>
2025-04-07 07:49:26 +0300

    Update behavior_turbo_key.c

--- app/src/behaviors/behavior_turbo_key.c
+++ app/src/behaviors/behavior_turbo_key.c
@@ -102,6 +102,7 @@ static void behavior_turbo_timer_handler(struct k_work *item) {
     //LOG_DBG("Turbo timer reached.");
     struct zmk_behavior_binding_event event = {.position = data->position,
                                                .timestamp = k_uptime_get()};
+    data->start_index = 0;
      for (int i = data->start_index; i < data->start_index + data->count; i++) {
     zmk_behavior_queue_add(event.position, data->bindings[i], true, data->tap_ms);
     zmk_behavior_queue_add(event.position, data->bindings[i], false, 0);

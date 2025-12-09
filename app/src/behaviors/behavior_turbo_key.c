ae501b4ffcd7e7f6c464ba07d1a3db571ed2af1a
ShineDoku <acidka1997@gmail.com>
2025-12-09 17:13:48 +0300

    Update behavior_turbo_key.c

--- app/src/behaviors/behavior_turbo_key.c
+++ app/src/behaviors/behavior_turbo_key.c
@@ -92,8 +92,7 @@ static void behavior_turbo_timer_handler(struct k_work *item) {
         zmk_behavior_queue_add(event.position, data->bindings[i], true, data->tap_ms);
         zmk_behavior_queue_add(event.position, data->bindings[i], false, 0);
     }
-    reset_timer(data, event);
-    k_work_schedule(d_work, K_MSEC(100));
+    reset_timer(data, event)
 }
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,

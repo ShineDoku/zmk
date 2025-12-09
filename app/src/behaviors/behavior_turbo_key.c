88a64e5f17c4196bc72a743fecd92aaf237a0acf
ShineDoku <acidka1997@gmail.com>
2025-12-09 16:33:59 +0300

    Update behavior_turbo_key.c

--- app/src/behaviors/behavior_turbo_key.c
+++ app/src/behaviors/behavior_turbo_key.c
@@ -36,7 +36,6 @@ struct behavior_turbo_data {
 
     int tap_ms;
     int wait_ms;
-    struct zmk_behavior_binding bindings[];
 
     // Timer Data
     bool timer_started;
@@ -44,6 +43,7 @@ struct behavior_turbo_data {
     bool turbo_decided;
     int64_t release_at;
     struct k_work_delayable release_timer;
+    struct zmk_behavior_binding bindings[];
 };
 
 static int behavior_turbo_key_init(const struct device *dev) { return 0; };
